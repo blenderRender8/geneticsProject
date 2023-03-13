00fc26f1bf2f81d8ab7d3bf1ad4617e802797b74
blenderRender <93060938+blenderRender8@users.noreply.github.com>
2023-03-13 12:15:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,109 @@
  
  
 */
+class Population {
+    constructor(){
+        this.pList = []; //list of dominant individuals
+        this.hList = []; //list of heterozygous individuals
+        this.qList = []; //list of reccesive individuals
+    }
+    addP = function(pClass){
+        this.pList.push(pClass);
+    }
+    addH = function(hClass){
+        this.hList.push(hClass);
+    }
+    addQ = function(qClass){
+        this.qList.push(qClass);
+    }
+    clear = function(){
+        const pList = []; //list of dominant individuals
+        const hList = []; //list of heterozygous individuals
+        const qList = []; //list of reccesive individuals
+    }
+};
+const population = new Population();
+population.addP(0);
+function drawStuff(pSize, hSize, qSize){
+    var ySpacing = 640 / 10;
+    var startingY = 64;
+    if (Math.round(pSize / 10) < 7){
+        var xSpacing = 64;
+    } else{
+        var xSpacing = (384 * 10)/ pSize;
+    }
+    var startingX = 0;
+    for (let i = 0; i < pSize; i++){
+        if (i % 10 == 0 && i != 0){
+            console.log("moving down");
+            startingX += xSpacing;
+            startingY = 64;
+        }
+        const pClass = {
+            type: 1, //1 is dominant
+            image : pImage,
+            drawMe : function() {
+                ctx = display.canvas.getContext("2d");
+                ctx.drawImage(this.image, startingX, startingY);
+            }
+        };
+        //pList.push(pClass);
+        population.addP(pClass);
+        pClass.drawMe();
+        startingY += ySpacing;
+
+    }
+    var ySpacing = 640 / 10;
+    var startingY = 64;
+    var xSpacing = (384 * 10)/ hSize;
+    var startingX = 448-16-8;
+    for (let i = 0; i < hSize; i++){
+        if (i % 10 == 0 && i != 0){
+            console.log("moving down");
+            startingX += xSpacing;
+            startingY = 64;
+        }
+        const hClass = {
+            type: 0, //0 is heterozygous
+            image : hImage,
+            drawMe : function() {
+                ctx = display.canvas.getContext("2d");
+                ctx.drawImage(this.image, startingX, startingY);
+            }
+        };
+        //hList.push(hClass);
+        population.addH(hClass);
+        hClass.drawMe();
+        startingY += ySpacing;
+    }
+    var ySpacing = 640 / 10;
+    var startingY = 64;
+    if (Math.round(qSize / 10) < 10){
+        var xSpacing = 64;
+    } else{
+        var xSpacing = (384 * 10)/ qSize;
+    }
+    var startingX = 1280-64;
+    for (let i = 0; i < qSize; i++){
+        if (i % 10 == 0 && i != 0){
+            console.log("moving down");
+            startingX -= xSpacing;
+            startingY = 64;
+        }
+        const qClass = {
+            type: -1, //-1 is recessive
+            image : qImage,
+            drawMe : function() {
+                ctx = display.canvas.getContext("2d");
+                ctx.drawImage(this.image, startingX, startingY);
+            }
+        };
+        //qList.push(qClass);
+        population.addQ(qClass);
+        qClass.drawMe();
+        startingY += ySpacing;
+    }
+}
 function pushButton(){
     p = parseFloat(document.getElementById("p").value);
     q = parseFloat(document.getElementById("q").value);
@@ -13,9 +116,9 @@ function pushButton(){
     if (p+q == 1 && populationSize >= 10 && populationSize <= 10000){ //fufills all requirements
         ctx = display.canvas.getContext("2d");
         ctx.clearRect(0, 0, display.canvas.width, display.canvas.height);
-        const pList = []; //list of dominant individuals
-        const hList = []; //list of heterozygous individuals
-        const qList = []; //list of reccesive individuals
+        //const pList = []; //list of dominant individuals
+        //const hList = []; //list of heterozygous individuals
+        //const qList = []; //list of reccesive individuals
         const factors = hwEquation(p, q);
         const pFactor = factors[0];
         const hFactor = factors[1]; //heterozygous
@@ -24,7 +127,7 @@ function pushButton(){
         var hSize = Math.round(hFactor * populationSize);
         var qSize = Math.round(qFactor * populationSize);
         
-
+        /*
         var ySpacing = 640 / 10;
         var startingY = 64;
         if (Math.round(pSize / 10) < 7){
@@ -100,9 +203,11 @@ function pushButton(){
             qClass.drawMe();
             startingY += ySpacing;
         }
-        console.log(pList);
-        console.log(hList);
-        console.log(qList);
+         */
+        drawStuff(pSize, hSize, qSize);
+        //console.log(pList);
+        //console.log(hList);
+        //console.log(qList);
         document.getElementById("pDisplay").innerHTML = "Ratio: " + (pSize / populationSize);
         document.getElementById("hDisplay").innerHTML = "Ratio: " + (hSize / populationSize);
         document.getElementById("qDisplay").innerHTML = "Ratio: " + (qSize / populationSize);
@@ -147,6 +252,9 @@ function startSimulation(){
     var dominant = 0;
     var heterozygous = 0;
     var recessive = 0;
+    const drawDom = [];
+    const drawHet = [];
+    const drawRec = [];
     //sampling w/ replacement
     for (let index = 0; index < populationSize; index++){
         allele1 = checkAllele(p);
@@ -155,6 +263,7 @@ function startSimulation(){
         population.push(sNum); // 1+1 = 2 is dominant, 1+0 = 1 is heterozygous, 0+0 = 0 is recessive
         if (sNum == 0){
             recessive++;
+            drawRec.push();
         } else if (sNum == 1){
             heterozygous++;
         } else if (sNum == 2){
@@ -165,7 +274,6 @@ function startSimulation(){
     console.log(dominant);
     console.log(heterozygous);
     console.log(recessive);
-    
 }
 function calculateNoReplacement(total, dom, het, rec, domdead, hetdead, recdead){
     dom *= (1 - domdead); //dominant individuals post thanos snap

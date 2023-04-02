03212c7fa46f840ab88efb4803f732c072b953db
ryanchan70 <91448275+ryanchan70@users.noreply.github.com>
2023-04-01 17:32:13 -0700

    sampling w/ replacement & slider functionality

--- script.js
+++ script.js
@@ -135,17 +135,20 @@ function calculateNoReplacement(total, dom, het, rec, domdead, hetdead, recdead)
     return [p_2, q_2]; //final values after thanos snap
 }
 
-function checkAllele(decimal, returnValue){
+function getAllele(decimal) {
     //returns 1 if dominant, 0 if recessive
     num = Math.random();
+    console.log(num + " < " + decimal);
     if (num < decimal){
+        console.log("return 1");
         return 1;
     }
+    console.log("return 0");
     return 0;
 }
-function pushButton(){
-    p = parseFloat(document.getElementById("p").value);
-    q = parseFloat(document.getElementById("q").value);
+function startPrediction(){
+    p = parseFloat(document.getElementById("p").innerHTML);
+    q = parseFloat(document.getElementById("q").innerHTML);
     console.log(p + " " + q);
     console.log(p + q);
     populationSize = parseFloat(document.getElementById("popSize").value);
@@ -163,94 +166,14 @@ function pushButton(){
         var hSize = Math.round(hFactor * populationSize);
         var qSize = Math.round(qFactor * populationSize);
         
-        /*
-        var ySpacing = 640 / 10;
-        var startingY = 64;
-        if (Math.round(pSize / 10) < 7){
-            var xSpacing = 64;
-        } else{
-            var xSpacing = (384 * 10)/ pSize;
-        }
-        var startingX = 0;
-        for (let i = 0; i < pSize; i++){
-            if (i % 10 == 0 && i != 0){
-                console.log("moving down");
-                startingX += xSpacing;
-                startingY = 64;
-            }
-            const pClass = {
-                type: 1, //1 is dominant
-                image : pImage,
-                drawMe : function() {
-                    ctx = display.canvas.getContext("2d");
-                    ctx.drawImage(this.image, startingX, startingY);
-                }
-            };
-            pList.push(pClass);
-            pClass.drawMe();
-            startingY += ySpacing;
-
-        }
-        var ySpacing = 640 / 10;
-        var startingY = 64;
-        var xSpacing = (384 * 10)/ hSize;
-        var startingX = 448-16-8;
-        for (let i = 0; i < hSize; i++){
-            if (i % 10 == 0 && i != 0){
-                console.log("moving down");
-                startingX += xSpacing;
-                startingY = 64;
-            }
-            const hClass = {
-                type: 0, //0 is heterozygous
-                image : hImage,
-                drawMe : function() {
-                    ctx = display.canvas.getContext("2d");
-                    ctx.drawImage(this.image, startingX, startingY);
-                }
-            };
-            hList.push(hClass);
-            hClass.drawMe();
-            startingY += ySpacing;
-        }
-        var ySpacing = 640 / 10;
-        var startingY = 64;
-        if (Math.round(qSize / 10) < 10){
-            var xSpacing = 64;
-        } else{
-            var xSpacing = (384 * 10)/ qSize;
-        }
-        var startingX = 1280-64;
-        for (let i = 0; i < qSize; i++){
-            if (i % 10 == 0 && i != 0){
-                console.log("moving down");
-                startingX -= xSpacing;
-                startingY = 64;
-            }
-            const qClass = {
-                type: -1, //-1 is recessive
-                image : qImage,
-                drawMe : function() {
-                    ctx = display.canvas.getContext("2d");
-                    ctx.drawImage(this.image, startingX, startingY);
-                }
-            };
-            qList.push(qClass);
-            qClass.drawMe();
-            startingY += ySpacing;
-        }
-         */
         drawStuff(pSize, hSize, qSize);
-        //console.log(pList);
-        //console.log(hList);
-        //console.log(qList);
-        document.getElementById("pDisplay").innerHTML = "Ratio: " + (pSize / populationSize);
-        document.getElementById("hDisplay").innerHTML = "Ratio: " + (hSize / populationSize);
-        document.getElementById("qDisplay").innerHTML = "Ratio: " + (qSize / populationSize);
+        //document.getElementById("pDisplay").innerHTML = "Ratio: " + (pSize / populationSize);
+        //document.getElementById("hDisplay").innerHTML = "Ratio: " + (hSize / populationSize);
+        //document.getElementById("qDisplay").innerHTML = "Ratio: " + (qSize / populationSize);
         ctx = display.canvas.getContext("2d");
         ctx.font = "24px Avenir Next";
         ctx.fillText("Predicted Population:", 4, 20);
-        ctx.font = "20px serif";
+        ctx.font = "20px Avenir Next";
         ctx.fillText("Homozygous Dominant: " + pSize + " individuals", 20, 48);
         ctx.fillText("Heterozygous: " + hSize + " individuals", 500, 48);
         ctx.fillText("Homozygous Recessive: " + qSize + " individuals", 900, 48);
@@ -278,8 +201,9 @@ function draw(text, fontSize, fontName, x, y) {
 }
 
 function startSimulation(){
-    p = parseFloat(document.getElementById("p").value);
-    q = parseFloat(document.getElementById("q").value);
+    p = parseFloat(document.getElementById("p").innerHTML);
+    q = parseFloat(document.getElementById("q").innerHTML);
+    console.log("p and q are " + p + " " + q);
     populationSize = parseFloat(document.getElementById("popSize").value);
     totalAlleles = populationSize * 2;
     dominantAlleles = Math.round(totalAlleles * p);
@@ -288,13 +212,10 @@ function startSimulation(){
     var dominant = 0;
     var heterozygous = 0;
     var recessive = 0;
-    const drawDom = [];
-    const drawHet = [];
-    const drawRec = [];
     //sampling w/ replacement
     for (let index = 0; index < populationSize; index++){
-        allele1 = checkAllele(p);
-        allele2 = checkAllele(p);
+        allele1 = getAllele(p);
+        allele2 = getAllele(p);
         sNum = allele1 + allele2;
         if (sNum == 0){
             recessive++;
@@ -310,9 +231,9 @@ function startSimulation(){
     console.log(recessive);
     drawStuff(dominant, heterozygous, recessive);
     
-    document.getElementById("pDisplay").innerHTML = "Ratio: " + (dominant / populationSize);
-    document.getElementById("hDisplay").innerHTML = "Ratio: " + (heterozygous / populationSize);
-    document.getElementById("qDisplay").innerHTML = "Ratio: " + (recessive / populationSize);
+    document.getElementById("hdDisplay").innerHTML = "Ratio: " + (dominant / populationSize);
+    document.getElementById("heteroDisplay").innerHTML = "Ratio: " + (heterozygous / populationSize);
+    document.getElementById("hrDisplay").innerHTML = "Ratio: " + (recessive / populationSize);
     ctx = display.canvas.getContext("2d");
     ctx.font = "24px Avenir Next";
     ctx.fillText("Population (Selection w/ Replacement):", 4, 20);
@@ -325,4 +246,33 @@ function startSimulation(){
 
 function thanosSnap(){
     
+}
+
+$(function() {
+	var rangePercent = $('[type="range"]').val();
+
+	$('[type="range"]').on('change input', function() {
+		rangePercent = Math.floor($('[type="range"]').val()*100);
+
+        var dominant = rangePercent/100;
+        var recessive = 1-dominant;
+
+        var homodominant = dominant ** 2; // ** is exponent
+        var heterozygous = 2 * dominant * recessive;
+        var homorecessive = recessive ** 2;
+
+		$('h4').html(rangePercent + "/" + (100-rangePercent) +'<span></span>');
+		$('[type="range"], h4>span').css('filter', 'hue-rotate(' + (rangePercent) + 'deg)');
+		$('h4').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/150)) + ')', 'left': rangePercent+'%'});
+	
+        document.getElementById("hdDisplay").innerHTML = Math.round(homodominant * 10000) / 100; 
+        document.getElementById("heteroDisplay").innerHTML = Math.round(heterozygous * 10000) / 100;
+        document.getElementById("hrDisplay").innerHTML = Math.round(homorecessive * 10000) / 100;
+        document.getElementById("p").innerHTML = Math.round(dominant*100)/100;
+        document.getElementById("q").innerHTML = Math.round(recessive*100)/100;
+    });
+});
+
+function randomInt(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
